1554014a06ecba049d7269e2e05f4ce475d63224
Christian Saron <chnsaron@gmail.com>
2024-03-24 13:34:21 +0800

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCXYV4o_SG8nQFw2dUfgfwFjiWNhxlb6ik",
+  authDomain: "deli-client-76c6b.firebaseapp.com",
+  projectId: "deli-client-76c6b",
+  storageBucket: "deli-client-76c6b.appspot.com",
+  messagingSenderId: "539656130603",
+  appId: "1:539656130603:web:2017f6e7be5f984cf61a6e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
